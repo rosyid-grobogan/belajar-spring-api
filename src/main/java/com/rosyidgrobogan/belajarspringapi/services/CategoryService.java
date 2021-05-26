06c7047f31831948c4e334019cec8dbef5ce5207
Rosyid <rosyidgrobogan@gmail.com>
2021-05-26 19:26:28 +0700

    + Category Service

---
+++ src/main/java/com/rosyidgrobogan/belajarspringapi/services/CategoryService.java
@@ -0,0 +1,41 @@
+package com.rosyidgrobogan.belajarspringapi.services;
+
+import com.rosyidgrobogan.belajarspringapi.models.enities.Category;
+import com.rosyidgrobogan.belajarspringapi.models.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CategoryService {
+
+    private final CategoryRepository categoryRepo;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepo) {
+        this.categoryRepo = categoryRepo;
+    }
+
+    public Category save(Category category) {
+        return categoryRepo.save(category);
+    }
+
+    public Category findOne(Long id){
+        Optional<Category> category = categoryRepo.findById(id);
+        if (!category.isPresent()) {
+            return null;
+        }
+        return category.get();
+    }
+
+    public Iterable<Category> findAll(){
+        return categoryRepo.findAll();
+    }
+
+    public void removeOne(Long id){
+        categoryRepo.deleteById(id);
+    }
+}
