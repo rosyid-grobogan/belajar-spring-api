4781b83d5f3d4a0837cecb044dd56130a85b6943
Rosyid <rosyidgrobogan@gmail.com>
2021-05-26 12:38:46 +0700

    + Product Repository

---
+++ src/main/java/com/rosyidgrobogan/belajarspringapi/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.rosyidgrobogan.belajarspringapi.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository {
+}
